dd18b56ee9998c3bc6542cc27b130fdc238024a7
Ethan Water <watermonic@gmail.com>
2024-01-09 00:18:39 -0500

    Merge Nodes

--- linkednodes/floyds_cycle.go
+++ linkednodes/floyds_cycle.go
@@ -1,11 +1,5 @@
 package linkednodes
 
-type LinkedNode struct {
-	Val int
-	Next *LinkedNode
-}
-
-
 func FloydsCycle(head *LinkedNode) bool {
 	slowptr, fastptr := head, head
 	for slowptr!=nil && fastptr!=nil && fastptr.Next!=nil{
--- linkednodes/linked_nodes.go
+++ linkednodes/linked_nodes.go
@@ -5,6 +5,11 @@ type LinkedList[T any] struct {
 	Next  *LinkedList[T]
 }
 
+type LinkedNode struct {
+	Val int
+	Next *LinkedNode
+}
+
 func (node *LinkedList[T]) Fetch() []T {
 	if node == nil {
 		return nil
@@ -16,3 +21,41 @@ func (node *LinkedList[T]) Fetch() []T {
 
 	return stack
 }
+
+func (node *LinkedNode) Merge(node2 *LinkedNode) *LinkedNode {
+	sentinel_node := &LinkedNode{}
+	merging_node := sentinel_node
+
+	switch{
+	case node2 == nil:
+		return node
+	case node == nil:
+		return node2
+	}
+
+	for node!=nil && node2!=nil{
+		switch{
+		case node.Val > node2.Val:
+			merging_node.Next = node
+			node = node.Next
+		case node.Val < node2.Val:
+			merging_node.Next = node2
+			node2 = node2.Next
+		}
+		merging_node = merging_node.Next
+	}
+
+	switch{
+	case node == nil && node2 != nil:
+		merging_node.Next = node2
+	case node != nil && node2 == nil:
+		merging_node.Next = node
+	}
+
+	return sentinel_node
+}
+
+func (node *LinkedNode) InplaceMerge(node2 *LinkedNode) {
+
+}
+
